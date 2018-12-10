dfce2c0dbaa09b68d09bbd6b75f8299b2b089ac7
Andrew K <andrewk@Andrew-Ks-iMac.local>
2018-12-10 12:54:00 +0400

    minor update get_perc.R

--- R/get_perc.R
+++ R/get_perc.R
@@ -4,8 +4,8 @@ get_perc <- function(data){
 
   data <- select_if(data, is.numeric)
 
-  percentiles <- data %>% map_df(~quantile(.x, probs = c(0.01, 0.25, 0.5, 0.75, 0.99), na.rm = TRUE))
-  iqr <- data %>% map_df(~IQR(.x, na.rm = TRUE))
+  percentiles <- data %>% map_dfc(~quantile(.x, probs = c(0.01, 0.25, 0.5, 0.75, 0.99), na.rm = TRUE))
+  iqr <- data %>% map_dfc(~IQR(.x, na.rm = TRUE))
 
   key <- data_frame(Key = c("Perc 1", "Perc 25", "Perc 50", "Perc 75", "Perc 99", "IQR"))
 

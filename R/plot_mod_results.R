17eefca9077dd1218a2d41227a6655986952cc8f
Andrew K <andrewk@Andrew-Ks-iMac.local>
2018-12-20 01:39:00 +0400

    visualization changes to plot_mod_results

--- R/plot_mod_results.R
+++ R/plot_mod_results.R
@@ -23,18 +23,20 @@ plot_mod_results <- function(data, mod_names = NULL, plot_cols = NULL, plot_rows
       stat_summary(fun.y = "mean", geom = "point", shape = 23, fill = "red") +
       labs(title = "Model Performance Comparison") +
       theme_light() +
-      theme(axis.title.x = element_blank())
+      theme(axis.title.x = element_blank(),
+            strip.text.x = element_text(color = "black", size = rel(1.1)),
+            strip.background = element_blank())
   } else if (conf_int95 == TRUE) {
     ggplot(data, aes(Model, Value)) + geom_boxplot(fill = "lightgray") +
       facet_wrap(~Metric, scales = "free_y", ncol = plot_cols, nrow = plot_rows) +
-      stat_summary(
-        fun.data = mean_se, geom = "errorbar",
-        color = "red", width = 0.33, fun.args = list(mult = 1.96)
-      ) +
+      stat_summary(fun.data = mean_se, geom = "errorbar",
+                   color = "red", width = 0.33, fun.args = list(mult = 1.96)) +
       stat_summary(fun.y = "mean", geom = "point", shape = 23, fill = "red") +
       labs(title = "Model Performance Comparison") +
       theme_light() +
-      theme(axis.title.x = element_blank())
+      theme(axis.title.x = element_blank(),
+            strip.text.x = element_text(color = "black", size = rel(1.1)),
+            strip.background = element_blank())
   } else {
     stop("The argument conf_int95 can take the values TRUE or FALSE only.")
   }
